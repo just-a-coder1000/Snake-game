8d6c2c1fb630ac3c13a1c52930378a0771f8b656
just-a-coder1000 <74711827+just-a-coder1000@users.noreply.github.com>
2020-12-21 16:42:02 +0530

    Add files via upload

---
+++ snake game/sketch.js
@@ -0,0 +1,45 @@
+var s;
+var scl = 20;
+var food;
+
+function setup() {
+  createCanvas(600, 600);
+  s = new snake();
+  frameRate(10);
+  food = createVector(random(width), random(height))
+  pickLocation();
+}
+
+function pickLocation(){
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function draw() {
+  background("black");
+  
+  if(s.eat(food)){
+    pickLocation();
+  }
+  
+   s.death();
+   s.update();
+   s.show();
+  
+  fill("red");
+  rect(food.x, food.y, scl,scl)
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    s.dir(0,-1);
+  }else if(keyCode === DOWN_ARROW){
+    s.dir(0,1);
+  }else if(keyCode === RIGHT_ARROW){
+    s.dir(1,0);
+  }else if(keyCode === LEFT_ARROW){
+    s.dir(-1,0);
+  }
+}
